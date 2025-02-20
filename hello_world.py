03fda914b26e8a9476621519e131b67fb241fb35
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-02-20 12:47:14 +0100

    Added my first python scripts

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+print("Hello World")
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue = 1
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue = 3.14
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue = 5j
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue = True
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
