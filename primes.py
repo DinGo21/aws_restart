296db12ed5f1a16dc7b64a2639d96543e25b41ca
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-02-24 14:12:34 +0100

    Added primes.py that prints out prime numbers from 1 to 250

---
+++ primes.py
@@ -0,0 +1,19 @@
+def	isPrime(n):
+	i = 2
+
+	while i < n:
+		if n % i == 0:
+			return 0
+		i += 1
+	return 1
+
+def printPrimes():
+	outfile = open("result.txt", "w")
+
+	for i in range(1, 251):
+		if isPrime(i):
+			outfile.write(str(i) + '\n')
+	outfile.close()
+
+if __name__ == "__main__":
+	printPrimes()
